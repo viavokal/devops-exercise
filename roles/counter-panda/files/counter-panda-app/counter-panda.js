b647553a20f9f9e09d7e633d1025984d804efdad
viavokal <avraham.kalvo@gmail.com>
2017-04-27 13:04:06 +0300

    Addded comments and extra fixes

--- roles/counter-panda/files/counter-panda-app/counter-panda.js
+++ roles/counter-panda/files/counter-panda-app/counter-panda.js
@@ -1,3 +1,13 @@
+/*
+
+ Name:   counter-panda
+ Author: Avraham Kalvo
+ Date:   25/4/2017
+
+ The following program listens to HTTP requests and prints out the amount of POST requests it received on every GET requested serviced
+
+ */
+
 var http = require('http');
 var config = require('./config.json');
 var HttpDispatcher = require('httpdispatcher');
@@ -14,21 +24,17 @@ function handleRequest(request, response){
 }
 
 dispatcher.onPost("/PostRequest", function(req, res) {
-    PostsCounter++
-    console.log(PostsCounter);
+    PostsCounter++   // Top the Posts counter by one whenever a Post request is received
+    console.log("POST Request Received, Current Requests: " + PostsCounter);
     res.end('PostsCounter');
 });
 
 dispatcher.onGet("/", function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Post Requests Current Counter: ' + PostsCounter);
+    res.end('POST Requests Current Counter: ' + PostsCounter);
 });
 
 dispatcher.onError(function(req, res) {
     res.writeHead(404);
     res.end("404 - Page Does not exists");
-});
-
-http.createServer(handleRequest).listen(config.port, function(){
-    console.log("Server listening on: http://localhost:%s", config.port);
 });
